020d509cf8efc0ba19dc07c484c3a35f22ce4a01
gordislava <gordislava@gmail.com>
2019-11-19 15:30:25 +0300

    'start'

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("source/scss/first-screen.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+    return gulp.src("source/img/**/*.{png,jpg,svg}")
+      .pipe(imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo()
+      ]))
+      .pipe(gulp.dest("source/img"));
+  });
+  
+  gulp.task("webp", function () {
+    return gulp.src("source/img/**/*.{png,jpg}")
+      .pipe(webp({ quality: 90 }))
+      .pipe(gulp.dest("source/img"));
+  });
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/scss/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("css", "server"));
