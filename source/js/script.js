2a020e97e48b73e1c20ce6ccbb4207caff165fc0
Gordislava <gordislava@gmail.com>
2019-11-24 20:33:26 +0300

    header, glide-slider, footer

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+// var input = document.querySelector('#options-gap-input')
+
+// var glide = new Glide('.glide', {
+//   gap: input.value,
+//   perView: 4
+// })
+
+// input.addEventListener('input', function (event) {
+//   glide.update({
+//     gap: event.target.value
+//   })
+// })
+
+// glide.mount()
